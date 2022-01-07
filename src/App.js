81c99c3b777810ca2d2d83db6d2a54a32362feef
ing928 <kik928@nate.com>
2022-01-07 16:57:03 +0900

    Practice (Coin Converter)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState(0);
+  const [btc, setBtc] = useState(0);
+  const [possible, setPossible] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []); //이 코드는 시작할때 한번만 랜더링.
   const onChange = (event) => {
-    setToDo(event.target.value);
+    setUsd(event.target.value); //내가 갖고 있는 dollar amount
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); //현재 todos 배열에 새로운 todo 추가.
-    setToDo("");
+  const onSelect = (event) => {
+    setBtc(event.target.value); //select한 coin의 가격
   };
-
+  const onClick = () => {
+    setPossible(Math.floor(usd / btc));
+  };
   return (
     <div>
-      <h1>Task: {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={toDo}
-          onChange={onChange}
-          placeholder="What's your today's plan?"
-        />
-        <br />
-        <button>Add</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+      <h1>Coins! {loading ? null : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading ...</strong> : null}
+      <select onChange={onSelect}>
+        {coins.map((coin) => (
+          <option value={coin.quotes.USD.price}>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>
         ))}
-      </ul>
+      </select>
+      <hr />
+      <input
+        type="number"
+        placeholder="convert USD"
+        value={usd}
+        onChange={onChange}
+      />
+      $
+      <br />
+      <button onClick={onClick}>Convert</button>
+      <br />
+      <input value={possible} disabled="true"></input>
     </div>
   );
 }
